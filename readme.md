fc93c494d8c1c7294c34706161ebd8a4a3ba41d1
MyCallAngel0 <144151303+MihaiFAF-221@users.noreply.github.com>
2023-10-04 15:31:43 +0300

    Added a Readme.md file
    Readme will be updated later

---
+++ readme.md
@@ -0,0 +1,9 @@
+# OOP laboratory FAF-221
+
+## Laboratory 0
+Basic laboratory task
+
+## Laboratory 1
+To create a TUM Management System
+
+--might update later
---
+++ src/main/java/org/oop/secondlab/Main.java
@@ -0,0 +1,10 @@
+package org.oop.secondlab;
+
+import org.oop.secondlab.behaviour.ApplicationLoop;
+
+public class Main {
+    public static void main(String[] args) {
+        ApplicationLoop app = new ApplicationLoop();
+        app.run();
+    }
+}
